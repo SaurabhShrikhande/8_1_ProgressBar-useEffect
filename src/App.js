8b747e74b4f1d87fc3fd15ba245eec4bbb8267ec
Saurabh Shrikhande <saurabhshrikhande200@gmail.com>
2023-12-27 15:01:10 +0530

    extra added color & refresh button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+    const [filled , setfilled] = useState(0);
+     const [loading, setrunning] = useState("loading...");
+
+
+   useEffect(()=> {
+  if(filled < 100){
+    setTimeout(() => setfilled(prev => prev += 1), 50)
+  }
+  if(filled === 100){
+    setrunning("completed!")
+  }
+
+   }, [filled])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Progress Bar</h1>
+      <div className="bar" >
+          <div className="fill" style={{width:`${filled}%` , backgroundColor : filled < 35 ? "red" : "green"}}>
+                  <p style={{height:"100%", margin:"0" , marginLeft:"240px", color: filled < 50 ? "black":"white"}}>{filled}</p>  
+          </div>
+      </div>
+          <div style={{margin:"5px"}} >{loading}</div>
+          <button style={{margin: "10px", borderRadius:"10px", padding:"5px 15px",}}
+          onClick={() => setfilled(0)}> Refresh</button>
     </div>
+      
   );
 }
 
